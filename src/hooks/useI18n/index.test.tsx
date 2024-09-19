33bb63eb14f001302c710a75d68a4fa32fdc6491
joselrodrigues <ing.joserodrigues@gmail.com>
2024-09-19 00:19:12 -0300

    feat: add stryker mutator

--- src/hooks/useI18n/index.test.tsx
+++ src/hooks/useI18n/index.test.tsx
@@ -1,13 +1,20 @@
 import { createRoot } from "solid-js"
 
-import { useI18n } from "."
+import { Locale, useI18n } from "."
 
 vi.mock("@solid-primitives/i18n", () => ({
-  flatten: vi.fn((dict) => dict), // Mock de la función flatten
-  translator: vi.fn((dict) => vi.fn((key) => dict[key] || key)), // Mock de la función translator
+  flatten: vi.fn((dict) => dict),
+  translator: vi.fn((dict) => vi.fn((key) => dict[key] || key)),
 }))
 
 describe("useI18n", () => {
+  it("should locale be default to en", () => {
+    createRoot(() => {
+      const { locale } = useI18n()
+      expect(locale()).toBe("en")
+    })
+  })
+
   it("should return an object containing locale, setLocale, and t", () => {
     createRoot(() => {
       const result = useI18n()
@@ -23,6 +30,16 @@ describe("useI18n", () => {
     createRoot(() => {
       const { locale, setLocale } = useI18n()
       setLocale("es")
+      expect(locale()).toBe("es")
+    })
+  })
+
+  it("should keep locale unchanged if setLocale is called with invalid locale after valid one", () => {
+    createRoot(() => {
+      const { locale, setLocale } = useI18n()
+      setLocale("es")
+      expect(locale()).toBe("es")
+      setLocale("invalid-locale" as Locale)
       expect(locale()).toBe("es")
     })
   })
